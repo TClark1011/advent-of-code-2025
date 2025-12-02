d96265fdfc96c82cab2ab12f78b1ffd888dd892a
Thomas Clark <thomassiclark@gmail.com>
2025-12-02 18:48:37 +1100

    day 2 part 1

--- src/days/2/Puzzle.ts
+++ src/days/2/Puzzle.ts
@@ -1,9 +1,38 @@
 const first = (input: string) => {
-  console.log(input);
-  return 'solution 1';
+  const ids: number[] = [];
+
+  input
+    .split(',')
+    .map((rawIdRange) => rawIdRange.split('-').map(Number))
+    .forEach((range) => {
+      const min = range[0];
+      const max = range[1];
+
+      for (let i = min; i <= max; i++) {
+        ids.push(i);
+      }
+    });
+
+  const invalidIds = ids.filter((id) => {
+    const stringId = String(id);
+    if (stringId.length % 2 !== 0) return false;
+
+    const midpoint = stringId.length / 2;
+    const firstHalf = stringId.slice(0, midpoint);
+    const secondHalf = stringId.slice(midpoint);
+
+    return firstHalf === secondHalf;
+  });
+
+  let invalidIdsSum = 0;
+  invalidIds.forEach((id) => {
+    invalidIdsSum += id;
+  });
+
+  return invalidIdsSum;
 };
 
-const expectedFirstSolution = 'solution 1';
+const expectedFirstSolution = 1227775554;
 
 const second = (input: string) => {
   console.log(input);
