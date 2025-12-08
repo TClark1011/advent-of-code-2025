ec9ccc769157bbe18ac0de967b6b47a5ef8c2bd1
Thomas Clark <thomassiclark@gmail.com>
2025-12-08 17:55:15 +1100

    day 6 part 2

--- src/days/6/Puzzle.ts
+++ src/days/6/Puzzle.ts
@@ -39,10 +39,67 @@ const first = (input: string) => {
 const expectedFirstSolution = 4277556;
 
 const second = (input: string) => {
-  input;
-  return 'solution 2';
+  const inputRows = input.split('\n');
+  const operatorsRow = inputRows.pop()!;
+
+  const operators = operatorsRow.split(/\s+/).filter((v) => !!v);
+
+  const columnLengths: number[] = [];
+  let spaceStreak = 1;
+  operatorsRow.split('').forEach((character) => {
+    if (character !== ' ') {
+      if (spaceStreak === 1) return;
+      columnLengths.push(spaceStreak - 1);
+      spaceStreak = 1;
+      return;
+    }
+    spaceStreak++;
+  });
+  columnLengths.push(spaceStreak);
+
+  const columns: string[][] = [];
+  let parsedLength = 0;
+  for (let i = 0; i < columnLengths.length; i++) {
+    const column: string[] = [];
+    const columnLength = columnLengths[i];
+    inputRows.forEach((row) => {
+      column.push(row.slice(parsedLength, parsedLength + columnLength));
+    });
+    parsedLength += columnLength + 1;
+    columns.push(column);
+  }
+
+  let sum = 0;
+  columns.forEach((column, colIndex) => {
+    const columnLength = columnLengths[colIndex];
+    const operator = operators[colIndex] as '*' | '+';
+
+    let result = operator === '*' ? 1 : 0;
+    for (let digitIndex = 0; digitIndex < columnLength; digitIndex++) {
+      const digits: string[] = [];
+      column.forEach((value) => {
+        const digit = value.at(digitIndex)!;
+        if (!digit.trim()) return;
+        digits.push(digit);
+      });
+
+      const value = Number(digits.join(''));
+
+      switch (operator) {
+        case '*':
+          result *= value;
+          break;
+        case '+':
+          result += value;
+          break;
+      }
+    }
+    sum += result;
+  });
+
+  return sum;
 };
 
-const expectedSecondSolution = 'solution 2';
+const expectedSecondSolution = 3263827;
 
 export { expectedFirstSolution, expectedSecondSolution, first, second };
